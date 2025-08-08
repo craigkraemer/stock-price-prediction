a327d34946a5748d53f54eefac9eb3e67bdce0d3
CRAIG KRAEMER <craig.kraemer@protonmail.com>
2025-08-08 13:31:42 -0400

    Update stockPricePrediction.py

--- stockPricePrediction.py
+++ stockPricePrediction.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 data ={
-  "Day": [1, 2, 3, 4],
-  "Price": [101.23, 102.22, 102.62, 103.3]
+  "Day": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  "Price": [104.69, 103.48, 103.36, 102.27, 101.77, 101.12, 102.21, 102.45, 102.69,103.03]
 }
 df = pd.DataFrame(data)
 x = df[["Day"]] # Features
@@ -11,7 +11,7 @@ y= df["Price"] # Target
 
 model = LinearRegression()
 model.fit(x, y)
-prediction = model.predict([[5]])
+prediction = model.predict([[11]])
 print(df)
-print("Predicted price for Day 5:",
+print("Predicted price for Day 11:",
 prediction[0])
